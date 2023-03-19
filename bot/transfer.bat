f256e9517b81b3f13a9677c379919ea1a60399bb
mars <marshallpmiller@gmail.com>
2023-03-19 11:28:33 -0700

    hawnbot.txt move

--- bot/transfer.bat
+++ bot/transfer.bat
@@ -1,6 +1,7 @@
 cd C:\Users\marsh\ShawnBot\bot
 build.sh
 scp ShawnBot pi@192.168.1.77:/home/pi/shawnBot
-scp dojo.txt pi@192.168.1.77:/home/pi/shawnBot
+#scp dojo.txt pi@192.168.1.77:/home/pi/shawnBot
+scp shawn.txt pi@192.168.1.77:/home/pi/shawnBot
 pause
 del shawnBot
